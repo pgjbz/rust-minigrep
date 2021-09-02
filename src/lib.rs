5ae758a6d23bdc09ea982fab2b871dc02dfde80f
Paulo Gabriel Justino Bezerra <paulo.bezerra.ext@sascar.com.br>
2021-09-02 17:13:50 -0300

    Simple contains use

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let content: String = fs::read_to_string(config.filename)?;
 
-    println!("File content: \n{}", content);
+    // println!("File content: \n{}", content);
+	for line in search(config.query, &content) {
+		println!("{}", line);
+	}
     Ok(())
 }
 
@@ -21,5 +24,31 @@ impl Config<'_> {
         let query: &'a String = &args[1];
         let filename: &'a String = &args[2];
         Ok(Config {query: &query, filename: &filename})
+    }
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+	let mut results: Vec<&'a str> = Vec::<&'a str>::new();
+	for line in content.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
+	results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
