fb22b7e276f87ffd5bf2f74af64dcd56f713e761
Paulo Gabriel <pgj1085@gmail.com>
2021-09-05 19:01:47 -0300

    Print error on standard output and redirect success to another output using eprintln macro

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config= Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem on parse arguments: {}", err);
+        eprintln!("Problem on parse arguments: {}", err);
         process::exit(1);
     });
 
 	if let Err(e) = minigrep::is_valid_flags(&args) {
-		println!("{}", e);
+		eprintln!("{}", e);
 		process::exit(1);
 	}
 
@@ -18,7 +18,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
